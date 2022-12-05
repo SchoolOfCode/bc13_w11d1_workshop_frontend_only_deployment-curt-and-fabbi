2d2596fd4bc3246eeb5206e50c7012ee2643b571
OttCoder <101673336+Ottcoder@users.noreply.github.com>
2022-12-05 15:08:05 +0000

    Changed site text to test automatic deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          We are deploying on Netlify!
         </p>
         <a
           className="App-link"
